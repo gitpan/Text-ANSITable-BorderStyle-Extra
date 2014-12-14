02bf36c687b641ad3204a576959c0e292e89e6b1
perlancar <perlancar@gmail.com>
2014-12-14 11:58:41 +0000

    Import of PERLANCAR/Text-ANSITable-BorderStyle-Extra-0.03.tar.gz from CPAN.
    gitpan-cpan-distribution: Text-ANSITable-BorderStyle-Extra
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         PERLANCAR/Text-ANSITable-BorderStyle-Extra-0.03.tar.gz
    gitpan-cpan-author:       PERLANCAR
    gitpan-cpan-maturity:     released

--- lib/Text/ANSITable/BorderStyle/Demo.pm
+++ lib/Text/ANSITable/BorderStyle/Demo.pm
@@ -4,7 +4,7 @@ use 5.010;
 use strict;
 use warnings;
 
-our $VERSION = '0.02'; # VERSION
+our $VERSION = '0.03'; # VERSION
 
 our %border_styles = (
 
@@ -26,11 +26,11 @@ _
 1;
 # ABSTRACT: Demo border styles
 
-
 __END__
+
 =pod
 
-=encoding utf-8
+=encoding UTF-8
 
 =head1 NAME
 
@@ -38,38 +38,53 @@ Text::ANSITable::BorderStyle::Demo - Demo border styles
 
 =head1 VERSION
 
-version 0.02
+This document describes version 0.03 of Text::ANSITable::BorderStyle::Demo (from Perl distribution Text-ANSITable-BorderStyle-Extra), released on 2014-12-13.
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Text-ANSITable-BorderStyle-Extra>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-Text-ANSITable-BorderStyle-Extra>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=Text-ANSITable-BorderStyle-Extra>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
 
 =head1 AUTHOR
 
-Steven Haryanto <stevenharyanto@gmail.com>
+perlancar <perlancar@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2013 by Steven Haryanto.
+This software is copyright (c) 2014 by perlancar@cpan.org.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
-=head1 DESCRIPTION
-
-=head1 FUNCTIONS
-
-
-None are exported by default, but they are exportable.
-
 =head1 INCLUDED BORDER STYLES
 
-=over
+=head2 Demo::demo_custom_char
 
-=item * demo_custom_char (Demoes coderef in chars)
+Demoes coderef in chars (utf8: no, box_chars: no).
 
 
 Accept arguments C<char> (defaults to C<x>).
 
 
 
-=back
+ xxxxxxxxxxxxxxxxxxxxx
+ x column1 x column2 x
+ xxxxxxxxxxxxxxxxxxxxx
+ x row1.1  x row1.2  x
+ x row2.1  x row3.2  x
+ xxxxxxxxxxxxxxxxxxxxx
+ x row3.1  x row3.2  x
+ xxxxxxxxxxxxxxxxxxxxx
 
 =cut
-
--- lib/Text/ANSITable/BorderStyle/Extra.pm
+++ lib/Text/ANSITable/BorderStyle/Extra.pm
@@ -5,7 +5,7 @@ use strict;
 use utf8;
 use warnings;
 
-our $VERSION = '0.02'; # VERSION
+our $VERSION = '0.03'; # VERSION
 
 our %border_styles = (
 
@@ -78,9 +78,10 @@ our %border_styles = (
 # ABSTRACT: More border styles
 
 __END__
+
 =pod
 
-=encoding utf-8
+=encoding UTF-8
 
 =head1 NAME
 
@@ -88,39 +89,90 @@ Text::ANSITable::BorderStyle::Extra - More border styles
 
 =head1 VERSION
 
-version 0.02
+This document describes version 0.03 of Text::ANSITable::BorderStyle::Extra (from Perl distribution Text-ANSITable-BorderStyle-Extra), released on 2014-12-13.
+
+=head1 HOMEPAGE
+
+Please visit the project's homepage at L<https://metacpan.org/release/Text-ANSITable-BorderStyle-Extra>.
+
+=head1 SOURCE
+
+Source repository is at L<https://github.com/sharyanto/perl-Text-ANSITable-BorderStyle-Extra>.
+
+=head1 BUGS
+
+Please report any bugs or feature requests on the bugtracker website L<https://rt.cpan.org/Public/Dist/Display.html?Name=Text-ANSITable-BorderStyle-Extra>
+
+When submitting a bug or request, please include a test-file or a
+patch to an existing test-file that illustrates the bug or desired
+feature.
 
 =head1 AUTHOR
 
-Steven Haryanto <stevenharyanto@gmail.com>
+perlancar <perlancar@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-This software is copyright (c) 2013 by Steven Haryanto.
+This software is copyright (c) 2014 by perlancar@cpan.org.
 
 This is free software; you can redistribute it and/or modify it under
 the same terms as the Perl 5 programming language system itself.
 
-=head1 DESCRIPTION
-
-=head1 FUNCTIONS
-
-
-None are exported by default, but they are exportable.
-
 =head1 INCLUDED BORDER STYLES
 
-=over
+=head2 Extra::dash2
 
-=item * dash2 (Dash 2)
+Dash 2 (utf8: yes, box_chars: no).
 
-=item * dash3 (Dash 3)
+ ┌╌╌╌╌╌╌╌╌╌┬╌╌╌╌╌╌╌╌╌┐
+ ┆ column1 ╎ column2 ╎
+ ├╌╌╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌┤
+ ╎ row1.1  ╎ row1.2  ╎
+ ╎ row2.1  ╎ row3.2  ╎
+ ├╌╌╌╌╌╌╌╌╌┼╌╌╌╌╌╌╌╌╌┤
+ ╎ row3.1  ╎ row3.2  ╎
+ └╌╌╌╌╌╌╌╌╌┴╌╌╌╌╌╌╌╌╌┘
 
-=item * hboldh_dsingle (Horizontally-bold for header, single for data)
 
-=item * hdoubleh_dsingle (Horizontally-double for header, single for data)
+=head2 Extra::dash3
 
-=back
+Dash 3 (utf8: yes, box_chars: no).
+
+ ┌┄┄┄┄┄┄┄┄┄┬┄┄┄┄┄┄┄┄┄┐
+ ┆ column1 ┆ column2 ┆
+ ├┄┄┄┄┄┄┄┄┄┼┄┄┄┄┄┄┄┄┄┤
+ ┆ row1.1  ┆ row1.2  ┆
+ ┆ row2.1  ┆ row3.2  ┆
+ ├┄┄┄┄┄┄┄┄┄┼┄┄┄┄┄┄┄┄┄┤
+ ┆ row3.1  ┆ row3.2  ┆
+ └┄┄┄┄┄┄┄┄┄┴┄┄┄┄┄┄┄┄┄┘
+
+
+=head2 Extra::hboldh_dsingle
+
+Horizontally-bold for header, single for data (utf8: yes, box_chars: no).
+
+ ┌─────────┬─────────┐
+ │ column1 │ column2 │
+ ┝━━━━━━━━━┿━━━━━━━━━┥
+ │ row1.1  │ row1.2  │
+ │ row2.1  │ row3.2  │
+ ├─────────┼─────────┤
+ │ row3.1  │ row3.2  │
+ └─────────┴─────────┘
+
+
+=head2 Extra::hdoubleh_dsingle
+
+Horizontally-double for header, single for data (utf8: yes, box_chars: no).
+
+ ┌─────────┬─────────┐
+ │ column1 │ column2 │
+ ╞═════════╪═════════╡
+ │ row1.1  │ row1.2  │
+ │ row2.1  │ row3.2  │
+ ├─────────┼─────────┤
+ │ row3.1  │ row3.2  │
+ └─────────┴─────────┘
 
 =cut
-
